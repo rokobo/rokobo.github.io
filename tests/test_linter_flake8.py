3fa537f0c0fb0ef234964c8d42c767bee8a09adc
Kobos <61996066+rokobo@users.noreply.github.com>
2023-09-18 01:11:40 -0300

    Tests

--- tests/test_linter_flake8.py
+++ tests/test_linter_flake8.py
@@ -7,9 +7,36 @@ src_folder = os.path.join(workspace, "src")
 pages_folder = os.path.join(workspace, "src/pages")
 
 
-def test_flake8_functions_activity() -> None:
+def test_flake8_main() -> None:
     """Ensures main passes flake8 specifications."""
     file = os.path.join(src_folder, "main.py")
     result = flake8.get_style_guide().check_files([file])
-    # assert result.total_errors == 0, result.get_statistics(('F', 'E', 'W'))
-    assert True
+    assert result.total_errors == 0, result.get_statistics(('F', 'E', 'W'))
+
+
+def test_flake8_components() -> None:
+    """Ensures components passes flake8 specifications."""
+    file = os.path.join(src_folder, "components.py")
+    result = flake8.get_style_guide().check_files([file])
+    assert result.total_errors == 0, result.get_statistics(('F', 'E', 'W'))
+
+
+def test_flake8_about() -> None:
+    """Ensures about passes flake8 specifications."""
+    file = os.path.join(pages_folder, "about.py")
+    result = flake8.get_style_guide().check_files([file])
+    assert result.total_errors == 0, result.get_statistics(('F', 'E', 'W'))
+
+
+def test_flake8_certificates() -> None:
+    """Ensures certificates passes flake8 specifications."""
+    file = os.path.join(pages_folder, "certificates.py")
+    result = flake8.get_style_guide().check_files([file])
+    assert result.total_errors == 0, result.get_statistics(('F', 'E', 'W'))
+
+
+def test_flake8_navigation() -> None:
+    """Ensures navigation passes flake8 specifications."""
+    file = os.path.join(pages_folder, "navigation.py")
+    result = flake8.get_style_guide().check_files([file])
+    assert result.total_errors == 0, result.get_statistics(('F', 'E', 'W'))
--- tests/test_linter_pylint.py
+++ tests/test_linter_pylint.py
@@ -7,9 +7,36 @@ src_folder = os.path.join(workspace, "src")
 pages_folder = os.path.join(workspace, "src/pages")
 
 
-def test_pylint_functions_activity() -> None:
+def test_pylint_main() -> None:
     """Ensures main passes pylint specifications."""
     file = os.path.join(src_folder, "main.py")
     result = Run([file], exit=False).linter.stats
-    #assert result.global_note == 10, result.by_msg
-    assert True
+    assert result.global_note == 10, result.by_msg
+
+
+def test_pylint_components() -> None:
+    """Ensures components passes pylint specifications."""
+    file = os.path.join(src_folder, "components.py")
+    result = Run([file], exit=False).linter.stats
+    assert result.global_note == 10, result.by_msg
+
+
+def test_pylint_about() -> None:
+    """Ensures about passes pylint specifications."""
+    file = os.path.join(pages_folder, "about.py")
+    result = Run([file], exit=False).linter.stats
+    assert result.global_note == 10, result.by_msg
+
+
+def test_pylint_certificates() -> None:
+    """Ensures certificates passes pylint specifications."""
+    file = os.path.join(pages_folder, "certificates.py")
+    result = Run([file], exit=False).linter.stats
+    assert result.global_note == 10, result.by_msg
+
+
+def test_pylint_navigation() -> None:
+    """Ensures navigation passes pylint specifications."""
+    file = os.path.join(pages_folder, "navigation.py")
+    result = Run([file], exit=False).linter.stats
+    assert result.global_note == 10, result.by_msg
