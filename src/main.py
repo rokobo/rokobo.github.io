99bb140249c688276beb38431d9d229b1eb78b42
Kobos <61996066+rokobo@users.noreply.github.com>
2023-09-18 01:19:31 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,11 +5,8 @@ from dash_bootstrap_components.themes import BOOTSTRAP
 
 if __name__ == '__main__':
     app = Dash(
-        __name__,
-        update_title=None,
-        external_stylesheets=[BOOTSTRAP],
-        title="Pedro Kobori Portfolio",
-        assets_folder='../assets',
+        __name__, update_title=None, external_stylesheets=[BOOTSTRAP],
+        title="Pedro Kobori Portfolio", assets_folder='../assets',
         use_pages=True
     )
     app.run_server(port="8080")
