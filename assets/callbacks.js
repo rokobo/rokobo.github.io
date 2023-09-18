18c535d31c35140fbe9a34a3841aff63ba3ffc14
Kobos <61996066+rokobo@users.noreply.github.com>
2023-09-18 01:05:27 -0300

    New assets

---
+++ assets/callbacks.js
@@ -0,0 +1,12 @@
+if (!window.dash_clientside) {
+    window.dash_clientside = {};
+}
+
+window.dash_clientside.clientside = {
+    certificate_click: function(...args) {
+        const ctx = dash_clientside.callback_context.triggered[0]['prop_id'].substring(11, 14);
+        const modalOutputs = Array(args.length).fill(false);
+        modalOutputs[parseInt(ctx) - 1] = true;
+        return modalOutputs;
+    }
+}
